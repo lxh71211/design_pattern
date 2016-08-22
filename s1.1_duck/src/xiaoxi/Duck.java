00d175e92f71da3b85df89e0889ecc2870927d10
lxh71211 <363471211@qq.com>
2016-08-22 22:15:57 +0800

    strategy pattern

--- s1.1_duck/src/xiaoxi/Duck.java
+++ s1.1_duck/src/xiaoxi/Duck.java
@@ -8,6 +8,7 @@ public abstract class Duck {
 	public Duck() {
 	}
 	
+	//调用接口方法
 	public void performFly() {
 		flyBehavior.fly();
 	}
@@ -16,7 +17,20 @@ public abstract class Duck {
 		quackBehavior.quack();
 	}
 	
+	
 	public void swim() {
 		System.out.println("All ducks float, even decoys!");
 	}
+
+	
+	//设定方法--设定行为而不是在构造器内实例化
+	public void setFlyBehavior(FlyBehavior flyBehavior) {
+		this.flyBehavior = flyBehavior;
+	}
+
+	public void setQuackBehavior(QuackBehavior quackBehavior) {
+		this.quackBehavior = quackBehavior;
+	}
+	
+	
 }
--- s1.1_duck/src/xiaoxi/MiniDuckSimulator.java
+++ s1.1_duck/src/xiaoxi/MiniDuckSimulator.java
@@ -7,6 +7,13 @@ public class MiniDuckSimulator {
 		Duck mallard = new MallardDuck();
 		mallard.performFly();
 		mallard.performQuack();
+		
+		System.out.println("--------------------------------------");
+		
+		Duck model = new ModelDuck();
+		model.performFly();//调用方法会委托给flyBehavior对象，该对象在ModelDuck中设置
+		model.setFlyBehavior(new FlyRocketPowered());//动态改变
+		model.performFly();
 	}
 
 }
